ae5b6239df659360f585765fa10a4b5ae00d1f0e
huekoh <huexkoh@gmail.com>
2025-07-08 16:31:10 +0800

    Add hover draw effect to grids

--- script.js
+++ script.js
@@ -5,11 +5,19 @@ const totalWidth = 16 * (GRID_SIZE + 2);
 const container = document.querySelector(".grid-container");
 container.setAttribute("style", `max-width: ${totalWidth}px;`);
 
+// Function to change the background colour of a grid
+function changeColor(node) {
+    node.style.backgroundColor = "steelblue";
+}
 
 // Create and insert the grids into the container
 for (let i = 0; i < 16 * 16; i++) {
     const grid = document.createElement("div");
+
     grid.setAttribute("style", `width: ${GRID_SIZE}px; height: ${GRID_SIZE}px; 
-        border-style: solid; border-width: 1px;`);
+        border-style: solid; border-width: 1px; border-color: steelblue;`);
+
+    grid.addEventListener("mouseenter", () => changeColor(grid));
+    
     container.appendChild(grid);
 }
