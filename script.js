cee65d8daf3ac9a7d93a2a570b9a4c95236780e6
huekoh <huexkoh@gmail.com>
2025-07-08 17:50:28 +0800

    Add clear button mechanics

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 const GRID_SIZE = 24;
 const totalWidth = 16 * (GRID_SIZE + 2);
 
-// Select the div that will hold the grids
-const container = document.querySelector(".grid-container");
-container.setAttribute("style", `max-width: ${totalWidth}px;`);
-
 // Function to change the background colour of a grid
 function changeColor(node) {
     node.style.backgroundColor = "steelblue";
 }
 
 // Create and insert the grids into the container
-for (let i = 0; i < 16 * 16; i++) {
-    const grid = document.createElement("div");
+function createGrid() {
+    for (let i = 0; i < 16 * 16; i++) {
+        const grid = document.createElement("div");
 
-    grid.setAttribute("style", `width: ${GRID_SIZE}px; height: ${GRID_SIZE}px; 
-        border-style: solid; border-width: 1px; border-color: steelblue;`);
+        grid.setAttribute("style", `width: ${GRID_SIZE}px; height: ${GRID_SIZE}px; 
+            border-style: solid; border-width: 1px; border-color: steelblue;`);
 
-    grid.addEventListener("mouseenter", () => changeColor(grid));
+        grid.addEventListener("mouseenter", () => changeColor(grid));
     
-    container.appendChild(grid);
+        container.appendChild(grid);
+    }
 }
+
+function clearGrid() {
+    container.innerHTML = "";
+    createGrid();
+}
+
+// Set the div that will hold the grids
+const container = document.querySelector(".grid-container");
+container.setAttribute("style", `max-width: ${totalWidth}px;`);
+
+// Set the buttons
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", () => clearGrid());
+
+// Create intial grid
+createGrid();
