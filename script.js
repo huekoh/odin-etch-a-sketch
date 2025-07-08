c4c750d0aa9f52de59127284f7045ac5f2e89ecb
huekoh <huexkoh@gmail.com>
2025-07-08 20:03:02 +0800

    Add function to pen and erase mode buttons

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+// Global variables
 const GRID_SIZE = 24;
-const totalWidth = 16 * (GRID_SIZE + 2);
 
-// Function to change the background colour of a grid
-function changeColor(node) {
-    node.style.backgroundColor = "steelblue";
+// Set the div that will hold the grids
+const container = document.querySelector(".grid-container");
+const totalWidth = 16 * (GRID_SIZE + 2);
+container.setAttribute("style", `max-width: ${totalWidth}px;`);
+
+// Track the current drawing mode
+let currentMode = "pen";
+
+// Set mode to erase
+function setEraseMode(element) {
+    element.style.backgroundColor = "white";
 }
 
-// Create and insert the grids into the container
+// Set mode to pen
+function setPenMode(element) {
+    element.style.backgroundColor = "steelblue";
+}
+
+// Create and insert the grids into the container and default to pen mode
 function createGrid() {
     for (let i = 0; i < 16 * 16; i++) {
         const grid = document.createElement("div");
@@ -14,24 +27,43 @@ function createGrid() {
         grid.setAttribute("style", `width: ${GRID_SIZE}px; height: ${GRID_SIZE}px; 
             border-style: solid; border-width: 1px; border-color: steelblue;`);
 
-        grid.addEventListener("mouseenter", () => changeColor(grid));
+        grid.addEventListener("mouseenter", () => setPenMode(grid));
     
         container.appendChild(grid);
     }
 }
 
+// Clear the grid
 function clearGrid() {
     container.innerHTML = "";
     createGrid();
 }
 
-// Set the div that will hold the grids
-const container = document.querySelector(".grid-container");
-container.setAttribute("style", `max-width: ${totalWidth}px;`);
+// Change the mode of the grids to pen or eraser
+function changeMode() {
+    const grids = container.getElementsByTagName("div");
+    for (let i = 0; i < grids.length; i++) {
+        const grid = grids[i];
 
-// Set the buttons
+        // Add new eventListener
+        if (currentMode == "pen") {
+            grid.removeEventListener("mouseenter", () => setEraseMode(grid));
+            grid.addEventListener("mouseenter", () => setPenMode(grid));
+        } else {
+            grid.removeEventListener("mouseenter", () => setPenMode(grid));
+            grid.addEventListener("mouseenter", () => setEraseMode(grid));
+        }
+    }
+}
+
+// Declare and set the buttons
 const clear = document.querySelector(".clear");
+const erase = document.querySelector(".eraser");
+const pen = document.querySelector(".pen");
+
 clear.addEventListener("click", () => clearGrid());
+erase.addEventListener("click", () => {currentMode = "erase"; changeMode();});
+pen.addEventListener("click", () => {currentMode = "pen"; changeMode();});
 
 // Create intial grid
 createGrid();
